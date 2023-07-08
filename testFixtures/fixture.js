856cf0a85c7d238d81d8553731a10e69c5409a94
Jay Kishore Duvvuri <jaykishore96@ymail.com>
2023-07-08 21:36:32 +0300

    Added changes

--- testFixtures/fixture.js
+++ testFixtures/fixture.js
@@ -7,16 +7,23 @@ import CheckoutYourInformationPage from "../pages/checkoutYourInformationPage";
 import CheckoutOverviewPage from "../pages/checkoutOverviewPage";
 import CheckoutCompletePage from "../pages/checkoutCompletePage";
 
+const cp = require("child_process");
+const clientPlaywrightVersion = cp
+  .execSync("npx playwright --version")
+  .toString()
+  .trim()
+  .split(" ")[1];
+
 const caps = {
-  browser: 'chrome',
-  os: 'osx',
-  os_version: 'catalina',
-  name: 'Playwright-Sauce-Demo',
-  build: 'playwright-build-7',
-  'browserstack.username': 'jaykishoreduvvur_T1H2Kt',
-  'browserstack.accessKey':'TFVLzirFH3pFms9ATqso',
-  'browserstack.local': false,
-  'client.playwrightVersion': '1.35.1',
+  browser: "chrome",
+  os: "osx",
+  os_version: "catalina",
+  name: "Playwright-Sauce-Demo",
+  build: "playwright-build-7",
+  "browserstack.username": "jaykishoreduvvur_T1H2Kt",
+  "browserstack.accessKey": "TFVLzirFH3pFms9ATqso",
+  "browserstack.local": false,
+  "client.playwrightVersion": clientPlaywrightVersion,
 };
 
 // Patching the capabilities dynamically according to the project name.
@@ -26,17 +33,19 @@ const patchCaps = (name, title) => {
   let [browser, browser_version] = browerCaps.split(/@/);
   let osCapsSplit = osCaps.split(/ /);
   let os = osCapsSplit.shift();
-  let os_version = osCapsSplit.join(' ');
-  caps.browser = browser ? browser : 'chrome';
-  caps.browser_version = browser_version ? browser_version : 'latest';
-  caps.os = os ? os : 'osx';
-  caps.os_version = os_version ? os_version : 'catalina';
+  let os_version = osCapsSplit.join(" ");
+  caps.browser = browser ? browser : "chrome";
+  caps.os_version = browser_version ? browser_version : "latest";
+  caps.os = os ? os : "osx";
+  caps.os_version = os_version ? os_version : "catalina";
   caps.name = title;
 };
 
-const isHash = (entity) => Boolean(entity && typeof(entity) === "object" && !Array.isArray(entity));
-const nestedKeyValue = (hash, keys) => keys.reduce((hash, key) => (isHash(hash) ? hash[key] : undefined), hash);
-const isUndefined = val => (val === undefined || val === null || val === '');
+const isHash = (entity) =>
+  Boolean(entity && typeof entity === "object" && !Array.isArray(entity));
+const nestedKeyValue = (hash, keys) =>
+  keys.reduce((hash, key) => (isHash(hash) ? hash[key] : undefined), hash);
+const isUndefined = (val) => val === undefined || val === null || val === "";
 const evaluateSessionStatus = (status) => {
   if (!isUndefined(status)) {
     status = status.toLowerCase();
@@ -48,7 +57,7 @@ const evaluateSessionStatus = (status) => {
   } else {
     return "";
   }
-}
+};
 
 const test = fixture.extend({
   loginPage: async ({ page }, use) => {
